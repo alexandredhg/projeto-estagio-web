9b8cd381ff2330c1f995d2b5224916446e489e10
Alexandre Giovanini <alexandredhg@gmail.com>
2025-11-04 07:15:47 -0300

    Atualização .html

---
+++ src/acompanhamentoPaciente.ts
@@ -0,0 +1,10 @@
+const formAcompanhamentoPaciente = document.getElementById("formAcompanhamentoPaciente") as HTMLFormElement;
+
+const txtDataAtendimento = document.getElementById("txtDataAtendimento") as HTMLInputElement;
+const txtNomeTerapeuta = document.getElementById("txtNomeTerapeuta") as HTMLInputElement;
+const txtNomePaciente = document.getElementById("txtNomePaciente") as HTMLInputElement;
+const txtQueixaPaciente = document.getElementById("txtQueixaPaciente") as HTMLInputElement;
+const txtRelatorioPaciente = document.getElementById("txtRelatorioPaciente") as HTMLInputElement;
+
+const btnSubmit = document.getElementById("btnSubmit") as HTMLButtonElement;
+const divMensagem = document.getElementById("divMensagem") as HTMLDivElement;
