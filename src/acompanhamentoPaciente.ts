cb1798398dd658069a6da42bf748b5f3f5366606
Alexandre Giovanini <alexandredhg@gmail.com>
2025-11-16 23:22:28 -0300

    start do projeto

--- src/acompanhamentoPaciente.ts
+++ src/acompanhamentoPaciente.ts
@@ -1,9 +1,9 @@
 const formAcompanhamentoPaciente = document.getElementById("formAcompanhamentoPaciente") as HTMLFormElement;
 
+const txtNomePaciente = document.getElementById("txtNomePaciente") as HTMLInputElement;
 const txtDataAtendimento = document.getElementById("txtDataAtendimento") as HTMLInputElement;
 const txtNomeTerapeuta = document.getElementById("txtNomeTerapeuta") as HTMLInputElement;
-const txtNomePaciente = document.getElementById("txtNomePaciente") as HTMLInputElement;
-const txtQueixaPaciente = document.getElementById("txtQueixaPaciente") as HTMLInputElement;
+const txtSupervisorEstagio = document.getElementById("txtSupervisorEstagio") as HTMLInputElement;
 const txtRelatorioPaciente = document.getElementById("txtRelatorioPaciente") as HTMLInputElement;
 
 const btnSubmit = document.getElementById("btnSubmit") as HTMLButtonElement;
